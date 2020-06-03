e5f3bea27f76807221d89f09ce0a59191f57247b
ashuphatkare <58158389+ashuphatkare@users.noreply.github.com>
2020-06-03 20:36:02 +0530

    Updates

---
+++ precache-manifest.c2d643c479d19200fb77d7866f5060fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbde980d82b5400250b8d3dc1487065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5480544d75c965769de",
+    "url": "/static/css/main.36a20215.chunk.css"
+  },
+  {
+    "revision": "6d10393274e1c6a07da9",
+    "url": "/static/js/2.353d7998.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.353d7998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5480544d75c965769de",
+    "url": "/static/js/main.f5f6c529.chunk.js"
+  },
+  {
+    "revision": "5399181bf48f2256e3db",
+    "url": "/static/js/runtime-main.49be5b6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
